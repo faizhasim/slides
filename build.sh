a4032379990a7808f3c2e7a6946eaae3e6e0a45d
Mohd Faiz Hasim <faizhasim@gmail.com>
2014-12-16 19:44:41 +0800

    Improvements on build script

--- build.sh
+++ build.sh
@@ -5,17 +5,13 @@ if [ ! -d reveal.js/ ]; then
     rm 2.6.2.tar.gz
 fi
 
-docker run -v `pwd`:/source jagregory/pandoc -t revealjs -s /source/slides.md -o /source/slides.html
-
-sed "s/simple.css/default.css/g" slides.html \
+docker run -v `pwd`:/source jagregory/pandoc -t revealjs -s /source/slides.md \
+  | sed "s/simple.css/default.css/g" \
   | sed "s/007020/40B261/g" \
   | sed "s/06287e/88A8F9/g" \
   | sed "s/4070a0/8EC3F9/g" \
   | sed "s/902000/FFB412/g" \
   > index.html
-
-rm slides.html
-
 
 docker run -v `pwd`:/source jagregory/pandoc  -s /source/slides.md -o /source/functional-programming.pdf  --latex-engine=xelatex
 docker run -v `pwd`:/source jagregory/pandoc  -s /source/slides.md -o /source/functional-programming.epub  --latex-engine=xelatex
--- slides.md
+++ slides.md
@@ -2,7 +2,7 @@
 % Mohd Faiz Hasim
 % faiz.hasim@servicerocket.com • faizhasim@gmail.com
 
-# Some Info
+Some Info
 
 - Also available as [PDF](functional-programming.pdf), [EPUB](functional-programming.epub) and [MOBI](functional-programming.mobi) formats.
 - Hosted at [Github](https://github.com/faizhasim/faizhasim.github.io).
